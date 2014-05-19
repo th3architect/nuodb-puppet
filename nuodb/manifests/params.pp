39c4a441b559740002a69a0187281ba87ef8d935
Rich Kourtz <rkourtz@nuodb.com>
2014-05-19 13:16:52 -0400

    Version bump to 2.0.4

--- nuodb/manifests/params.pp
+++ nuodb/manifests/params.pp
@@ -3,10 +3,10 @@ class nuodb::params{
   $config_dir = "/opt/nuodb/etc"
   
   if $::osfamily == 'RedHat' or $::operatingsystem == 'amazon' {
-    $package_source = "http://download.nuohub.org/nuodb-2.0.3.linux.x64.rpm"
+    $package_source = "http://download.nuohub.org/nuodb-2.0.4.linux.x64.rpm"
     $package_provider = "rpm"
   } elsif $::osfamily == 'Debian' {
-    $package_source = "http://download.nuohub.org/nuodb-2.0.3.linux.x64.deb"
+    $package_source = "http://download.nuohub.org/nuodb-2.0.4.linux.x64.deb"
     $package_provider = "apt"
   } else {
     fail("Class['nuodb::params']: Unsupported osfamily: ${::osfamily}")
